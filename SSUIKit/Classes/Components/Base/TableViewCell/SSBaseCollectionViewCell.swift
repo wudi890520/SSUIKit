f0117232376cbd8d61f52c246754f9c651b215ee
wudi <593154883@qq.com>
2019-12-25 15:25:36 +0800

    commit 0.3.8

---
+++ SSUIKit/Classes/Components/Base/TableViewCell/SSBaseCollectionViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  SSBaseCollectionViewCell.swift
+//  BlocksKit
+//
+//  Created by 吴頔 on 2019/12/25.
+//
+
+import UIKit
+
+public class SSBaseCollectionViewCell: UICollectionViewCell {
+
+    override public func awakeFromNib() {
+        super.awakeFromNib()
+        addSubviews()
+        makeLayout()
+        // Initialization code
+    }
+
+}
+
+extension SSBaseCollectionViewCell {
+    @objc open func addSubviews() {
+        /// override by subclass
+    }
+    
+    @objc open func makeLayout() {
+        /// override by subclass
+    }
+}
+
+extension SSBaseCollectionViewCell {
+    @objc open func reloadCellData(_ data: Any) {
+        /// override by subclass
+    }
+}
---
+++ SSUIKit/Classes/Components/Base/TableViewCell/SSBaseTableViewCell.swift
@@ -0,0 +1,51 @@
+//
+//  SSBaseTableViewCell.swift
+//  BlocksKit
+//
+//  Created by 吴頔 on 2019/12/25.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+public class SSBaseTableViewCell: UITableViewCell {
+
+    public let line = UIView.line()
+    
+    public var indexPath: IndexPath?
+    
+    public var numberOfRowsInSection: Int = 0
+    
+    public var dispose = DisposeBag()
+    
+    override open func awakeFromNib() {
+        super.awakeFromNib()
+        addSubviews()
+        makeLayout()
+        // Initialization code
+    }
+
+    override open func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
+
+extension SSBaseTableViewCell {
+    @objc open func addSubviews() {
+        /// override by subclass
+    }
+    
+    @objc open func makeLayout() {
+        /// override by subclass
+    }
+}
+
+extension SSBaseCollectionViewCell {
+    @objc open func reloadData(_ data: Any) {
+        /// override by subclass
+    }
+}
