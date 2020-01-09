c5666dd136053458ce2fe22fb8a275b702c558ac
wudi <593154883@qq.com>
2020-01-09 22:20:00 +0800

    commit 0.5.6

---
+++ SSUIKit/Classes/Extensions/CollectionView.swift
@@ -0,0 +1,26 @@
+//
+//  CollectionView.swift
+//  DeviceKit
+//
+//  Created by 吴頔 on 2020/1/9.
+//
+
+import UIKit
+
+extension UICollectionView {
+    @discardableResult
+    func ss_register(_ className: AnyClass) -> Self {
+        let nibName = "\(className.description())".components(separatedBy: ".")
+        if let path = Bundle.init(for: className).path(forResource: nibName.first, ofType: "bundle") {
+            let bundle = Bundle.init(path: path)
+            let nib = UINib.init(nibName: nibName.last ?? "", bundle: bundle)
+            register(nib, forCellWithReuseIdentifier: nibName.last ?? "")
+            return self
+        }else{
+            let bundle = Bundle.init(for: className)
+            let nib = UINib.init(nibName: nibName.last ?? "", bundle: bundle)
+            register(nib, forCellWithReuseIdentifier: nibName.last ?? "")
+            return self
+        }
+    }
+}
